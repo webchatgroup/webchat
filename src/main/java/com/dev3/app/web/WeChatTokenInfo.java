30609c3fec5b4073743206f70b2626bab190eb19
Raymond Liu <kingswood0912@gmail.com>
2017-04-07 15:39:55 +0800

    changing package name

---
+++ src/main/java/com/dev3/app/web/WeChatTokenInfo.java
@@ -0,0 +1,40 @@
+package com.dev3.app.web;
+
+public class WeChatTokenInfo {
+	public String getSignature() {
+		return signature;
+	}
+
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getNonce() {
+		return nonce;
+	}
+
+	public void setNonce(String nonce) {
+		this.nonce = nonce;
+	}
+
+	public String getEchostr() {
+		return echostr;
+	}
+
+	public void setEchostr(String echostr) {
+		this.echostr = echostr;
+	}
+
+	private String signature;
+	private String timestamp;
+	private String nonce;
+	private String echostr;
+}
---
+++ src/main/java/com/dev3/app/web/package-info.java
@@ -0,0 +1,2 @@
+
+package com.dev3.app.web;
