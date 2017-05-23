9da0a4c3b37b970494274f93d007fe7895175242
Raymond Liu <kingswood0912@gmail.com>
2017-05-23 16:49:07 +0800

    Sorting suggestion list

--- src/main/java/com/dev3/app/web/controller/SuggestionController.java
+++ src/main/java/com/dev3/app/web/controller/SuggestionController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,57 +30,64 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 @Controller
 public class SuggestionController {
-	
+
 	private ISuggestionService suggestionService;
-	
-	
-	
+
 	@Autowired
 	public SuggestionController(ISuggestionService suggestionService) {
-		
+
 		this.suggestionService = suggestionService;
-		
+
 	}
-	
-	
-	@RequestMapping(value="/doCreateSuggestion", method=RequestMethod.POST,  produces="application/text")
+
+	@RequestMapping(value = "/doCreateSuggestion", method = RequestMethod.POST, produces = "application/text")
 	@ResponseBody
-	public String createSuggestion(@RequestParam(name="txtSuggestion", defaultValue="")String suggestion){
-		
+	public String createSuggestion(@RequestParam(name = "txtSuggestion", defaultValue = "") String suggestion) {
+
 		System.out.println("calling createSuggestion");
-		
+
 		Suggestion suggestionEntity = new Suggestion();
 		suggestionEntity.setContent(suggestion);
 		suggestionEntity.setCreateDate(new Date());
 		suggestionEntity.setStatus(Suggestion.STATUS_NEW);
-		
+
 		int id = suggestionService.addSuggestion(suggestionEntity);
 
 		Logger.debug("id is : " + id);
-		
+
 		return "ok";
 	}
-	
+
 	@RequestMapping("/suggestionList")
-	public String suggestionList(@PageableDefault(value = 10)Pageable pageable,Model model) {
-		
-		
-		Page<Suggestion> p = suggestionService.getSuggestions(pageable);
-		
-		List<Suggestion> suggestions = new ArrayList<>();
-		
-		Iterator<Suggestion> it = p.iterator();
-		
-		while(it.hasNext()){
-			suggestions.add(it.next());
+	public String suggestionList(
+			@PageableDefault(value = 10, direction = Sort.Direction.DESC, 
+			sort = { "createDate" }) Pageable pageable,
+			Model model) {
+
+		// Pageable p2 = new PageRequest(pageable.getPageNumber(),
+		// pageable.getPageSize(), Sort.Direction.DESC, "create_date");
+
+		try {
+			Page<Suggestion> p = suggestionService.getSuggestions(pageable);
+
+			List<Suggestion> suggestions = new ArrayList<>();
+
+			Iterator<Suggestion> it = p.iterator();
+
+			while (it.hasNext()) {
+				suggestions.add(it.next());
+			}
+
+			PageWrapper<Suggestion> pageWrapper = new PageWrapper<>(p, "suggestionList");
+
+			model.addAttribute("suggestions", suggestions);
+			model.addAttribute("page", pageWrapper);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		PageWrapper<Suggestion> pageWrapper = new PageWrapper<>(p, "suggestionList");
-		
-		model.addAttribute("suggestions", suggestions);
-		model.addAttribute("page", pageWrapper);
-		
-		return "suggestionList"; 
+
+		return "suggestionList";
 	}
-	
+
 }
